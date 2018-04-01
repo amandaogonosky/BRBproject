93b17640d1a6cd938db95b8272094bdc66756c6a
Jack <jackmidoglover@gmail.com>
2018-04-01 06:45:19 -0600

    function testing 5

--- javascript/brb.js
+++ javascript/brb.js
@@ -283,7 +283,8 @@ $("#addClose").on("click", function (event) {
 // Report Bike Stolen Functions: let user choose which bike was stolen
 $("#bike-bandit").on("click", function () {
   $(".stolenBike").css("visibility", "visible");
-  var usersRef = database.ref().child("users").child(userId);
+  var userBikes = database.ref().child("users").child(userId).val(); 
+  var usersRef = database.ref().child("users").child(userId).child(userBikes);
   usersRef.on("value", function (childSnapshot) {
     console.log(childSnapshot);
     for (let i = 0; i < childSnapshot.length; i++) {
